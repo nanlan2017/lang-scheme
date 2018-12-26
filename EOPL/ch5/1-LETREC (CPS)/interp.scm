58770f9f6227938e118099e41e911a901a7cc9a9
nanlan2017 <2543500257@qq.com>
2018-12-27 01:16:27 +0800

    ...

--- EOPL/ch5/1-LETREC (CPS)/interp.scm
+++ EOPL/ch5/1-LETREC (CPS)/interp.scm
@@ -5,11 +5,11 @@
   (require "data-structures.scm")
   (require "utils.scm")
   ;;============================================================= procedure  
-  ;; apply-procedure : Proc * Val -> ExpVal
-  (define (apply-procedure proc arg)
+  ;; apply-procedure : Proc * Val * Cont -> FinalAnswer
+  (define (apply-procedure/k proc arg cont)
     (cases Proc proc
       ($procedure (param bodyexp env)
-                  (eval bodyexp ($extend-env param arg env)))))
+                  (eval/k bodyexp ($extend-env param arg env) cont))))
 
   ;;============================================================= env
   ;; apply-env == look-up-env
@@ -29,28 +29,72 @@
   ;;============================================================ Continuation
   (define-datatype Continuation Continuation?
     ($end-cont)
-    
-    ($zero?-cont
-     (val ExpVal?)
+    ; zero?-exp
+    ($zero?-exp-cont
      (cont Continuation?))
-        
+    ; let-exp
+    ($let-exp-cont
+     (var identifier?)
+     (body expression?)
+     (env Env?)
+     (cont Continuation?))
+    ; if-exp
+    ($if-test-cont
+     (then-exp expression?)
+     (else-exp expression?)
+     (env Env?)
+     (cont Continuation?))
+    ; diff-exp
+    ($diff1-cont
+     (e2 expression?)
+     (env Env?)
+     (cont Continuation?))
+    ($diff2-cont
+     (v1 ExpVal?)
+     (cont Continuation?))
+    ; call-exp
+    ($rator-cont
+     (rand-exp expression?)
+     (env Env?)
+     (cont Continuation?))
+    ($rand-cont
+     (f-expval ExpVal?)
+     (cont Continuation?))        
     )
 
   ; type FinalAnswer = ExpVal
   ; apply-cont :: Cont -> ExpVal -> FinalAnswer
-  (define (apply-cont k val)
+  (define (apply-cont k VALUE) ; val : 上一步的计算结果
     (cases Continuation k
       ($end-cont ()
                  (eopl:printf "End of Computation.~%")
-                 val)
-      ($zero?-cont (expval cont)
-                   (apply-cont cont ($bool-val (zero? (expval->num expval)))))
-                   
+                 VALUE)
+      ($zero?-exp-cont (cont)
+                   (apply-cont cont ($bool-val (zero? (expval->num VALUE))))) ; VALUE : zero?(e)  中e的值
+      ($let-exp-cont (var body env cont)
+                     (eval/k body ($extend-env var VALUE env) cont))          ; VALUE : let x = e 中e的值
+      ($if-test-cont (then-exp else-exp env cont)
+                     (if (expval->bool VALUE)                                 ; VALUE : if e ..   中e的值
+                         (eval/k then-exp env cont)
+                         (eval/k else-exp env cont)))
+      ($diff1-cont (e2 env cont)
+                   (eval/k e2 env ($diff2-cont VALUE cont)))                  ; VALUE : -(e1,e2)  中e1的值
+      ($diff2-cont (v1 cont)
+                   (apply-cont cont ($num-val (- (expval->num v1) (expval->num VALUE))))); VALUE : -(e1,e2)  中e2的值
+      ($rator-cont (rand-exp env cont)
+                   (eval/k rand-exp env ($rand-cont VALUE cont)))             ; VALUE : (e1 e2)   中e1的值
+      ($rand-cont (f-expval cont)                                             
+                  (let [(f (expval->proc f-expval))]
+                    (apply-procedure/k f VALUE cont)                          ; VALUE : (e1 e2)   中e2的值
+                    ; (apply-cont cont (apply-procedure f VALUE))
+                    ))                                        
       ))  
   
-  ;;============================================================= eval
+  ;;============================================================= eval  
+  ; █████████ 重写目标： 所有对 eval/k的调用都属于tail call (doesn't build control context)
+  ; █████████ f(x,cont)  !=  cont( f(x,end-cont) )
+  
   ; eval :: Expression x Env x Cont -> FinalAnswer
-  ; 重写目标： 所有对 eval/k的调用都属于tail call (doesn't build control context)
   (define (eval/k exp env cont)
     (cases expression exp
       ;--------------------------------------------- no call on eval/k
@@ -67,58 +111,45 @@
       (proc-exp (var body)
                 (apply-cont cont ($proc-val ($procedure var body env))))
       ;--------------------------------------------- 1 call on eval
-      ; █████████ f(x,cont)  !=  cont( f(x,end-cont) )
-      
-      ; letrec : the inner-'eval' is in the same cont as caller-eval
-      
+      ; letrec : the inner-'eval' is in the same cont as caller-eval      
       ;      (letrec-exp (pid b-var p-body letrec-body)
       ;                  (eval letrec-body ($extend-env-rec pid b-var p-body env)))
       (letrec-exp (pid b-var p-body letrec-body)
                   (eval/k letrec-body ($extend-env-rec pid b-var p-body env) cont))
       
-      ; zero?-exp : eval --> [ expval->num >>> if zero?($) then ..else .. ] --> cont
-      
+      ; zero?-exp : eval --> [ expval->num >>= zero? >>= $bool-val ] --> cont      
       ;      (zero?-exp (e1)
-      ;                 (let [(v1 (expval->num (eval e1 env)))]
-      ;                   (if (zero? v1)
-      ;                       ($bool-val #t)
-      ;                       ($bool-val #f))))
+      ;                 (let [(☗ (eval e1 env))]
+      ;                   ($bool-val (zero? (expval->num ☗)))))
       (zero?-exp (e1)
-                 (eval/k e1 env ($zero?-cont cont)))
+                 (eval/k e1 env ($zero?-exp-cont cont)))
       ;--------------------------------------------- 2 call on eval
+      ;      (let-exp (var e1 body)
+      ;               (let [(☗ (eval e1 env))]
+      ;                 (eval body ($extend-env var ☗ env))))
+      (let-exp (var e1 body)
+               (eval/k e1 env ($let-exp-cont var body env cont)))
+      
+      ;      (if-exp (e1 e2 e3)
+      ;              (if (expval->bool (eval e1 env))
+      ;                  (eval e2 env)
+      ;                  (eval e3 env)))
+      (if-exp (e1 e2 e3)
+              (eval/k e1 env ($if-test-cont e2 e3 env cont)))
+      
       ;      (diff-exp (e1 e2)
       ;                (let [(num1 (expval->num (eval e1 env)))
       ;                      (num2 (expval->num (eval e2 env)))]
       ;                  ($num-val (- num1 num2))))
       (diff-exp (e1 e2)
-                (let [(num1 (expval->num (eval e1 env)))
-                      (num2 (expval->num (eval e2 env)))]
-                  ($num-val (- num1 num2))))
-      
-      ;      (let-exp (var e1 body)
-      ;               (let [(v1 (eval e1 env))]
-      ;                 (eval body ($extend-env var v1 env))))
-      (let-exp (var e1 body)
-               (let [(v1 (eval e1 env))]
-                 (eval body ($extend-env var v1 env))))
+                (eval/k e1 env ($diff1-cont e2 env cont)))      
       
       ;      (call-exp (rator rand)
       ;                (let [(f (expval->proc (eval rator env)))
       ;                      (arg (eval rand env))]
       ;                  (apply-procedure f arg)))
       (call-exp (rator rand)
-                (let [(f (expval->proc (eval rator env)))
-                      (arg (eval rand env))]
-                  (apply-procedure f arg)))
-      ;--------------------------------------------- 3 call on eval
-      ;      (if-exp (e1 e2 e3)
-      ;              (if (expval->bool (eval e1 env))
-      ;                  (eval e2 env)
-      ;                  (eval e3 env)))
-      (if-exp (e1 e2 e3)
-              (if (expval->bool (eval e1 env))
-                  (eval e2 env)
-                  (eval e3 env)))
+                (eval/k rator env ($rator-cont rand env cont)))
       ))
 
   ; eval-program :: Program -> FinalAnswer
